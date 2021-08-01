5943f68a2f9062c379220e3739c41ef53060e06d
Ji-Yeon Park <54522206+Ji-Yeon0107@users.noreply.github.com>
2021-08-02 06:07:31 +0900

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,6 +1,4 @@
-
 window.onload = function() { 
-
 $(function() {
     // 모달창
     $('.modal-btn').on("click", function() {
@@ -26,43 +24,40 @@ $(function() {
         $('.history-detail>img').css('opacity','0');
         $('.history-detail>img').eq(j).animate({opacity:'1'}, 600);
     });
-
-// 모바일
-    // 네비바 등장
-    $('.gnb-btn-bars_m').on("click", function() {
-        $('.gnb_m').css("display","block");
-        $('.gnb-btn-close_m').css("display","block");
-        $('.nav__depth2').css("display","none");
-
+    //(PC) snb
+    $('.snb-list').mouseenter(function() {
+        var i = $(this).index()-1;
+        var he = 40;
+        var listHeight = 40 *($(this).children().length-1) + he;
+        $(this).eq(i).css("height", listHeight);
     });
-    $('.gnb-btn-close_m').on("click", function() {
-        $('.gnb_m').css("display","none");
-        $(this).css("display", "none");
-    }); 
-    // 메뉴펼치기
 
-    $('.nav__depth1').on("click", function() {
-        $('.nav__depth2').hide();
-        $(this).children().slideToggle();
-    })        
-    $('.nav__depth2').on("click", function() {
-        $('.gnb_m').hide();
-        $('.gnb-btn-close_m').hide();
-
-    })     
-    // 서브페이지에서 메뉴클릭 컨텐츠 바뀌기
-    $('.menu-list').on("click", function() {
-        var i = $(this).index();
-        $('.main_m').hide();
-        $('.menu-list').css({"background":"none", "color":"black"});
-        $('.main_m').eq(i).css("display","block");
-        $(this).css({"background":"coral", "color":"white"});
+    $('.snb-list').mouseleave(function() {
+        var i = $(this).index()-1;
+        var he = 40;
+        $(this).eq(i).css("height", he);
     })
+        var removeEvent = $(function() {     
+        if (matchMedia("screen and (max-width:1200px)").matches) {
+            $('.snb-list').off("mouseenter");
+            $('.snb-list').off("mouseleave");
+        }
+    });
+ // (모바일) 서브페이지에서 메뉴클릭 
+        $('.snb-list').on("click", function() {
+                $('.snb-list').css("height","40px");
+                $('.snb-list-dep2').css("color","black");
+                $(this).css("height","122px");
+            })
+ 
+    })
+    
+ 
 
+
+    
     // 서브페이지 - 초코파이 하우스 - 제품 소개
     $('.img-pointer').on("click", function() {
-        
-
         if ($(this).children('.detail-all').css("opacity")=="0") {
             $(this).children('.detail-all').animate({opacity:"1"}, 400)
             $(this).children('.pointer-line').animate({opacity:"1"}, 50)
@@ -71,8 +66,80 @@ $(function() {
         } else {    
         }
     })
-})
+// 모바일
+
+    // 네비바 등장
+    $('.gnb-btn-bars_m').on("click", function() {
+        $('.gnb_m').show();
+        $('.gnb_m').animate({opacity:"1", right:"0"}, 500);
+        // $('.gnb_m').animate({right:"0"}, 1000);
+        $('.gnb-btn-close_m').animate({opacity:"1"}, 500);
+        $('.nav__depth2').css("display","none");
+        $(this).hide();
+
+    });
+    $('.gnb-btn-close_m').on("click", function() {
+        $('.gnb_m').animate({opacity:"0", right:"-50%"}, 500);
+        $(this).animate({opacity:"0"}, 200);
+        $('.gnb-btn-bars_m').show();
+    }); 
+    // 메뉴펼치기
+    $('.nav__depth1').on("click", function() {
+
+        $('.nav__depth1').css("color","white");
+        $('.nav__depth2').not($(this).children()).hide();
+        $(this).children('.nav__depth2').slideToggle(function() {
+            $(this).parents('.nav__depth1').css("color","coral");
+            $(this).css("color","white");
+        });
+    })        
+    $('.nav__depth2').on("click", function() {
+        $('.gnb_m').hide();
+        $('.gnb-btn-close_m').hide();
+
+    }) 
+    // 인덱스 메인 상단 이미지
+    var contentLength = $(".content_m").length;
+    var currentIndex = 0;
+    var current = 0;
+    var timeID;
+    timer();
+    function timer() {
+        timeID = setInterval(function(){
+            currentIndex++;
+            if(currentIndex >= contentLength) { currentIndex=0 };
+            showImage(currentIndex);
+        })
+    };
+    function showImage(i) {
+        $(".content_m").fadeOut(2000);
+        $(".content_m").eq(i).fadeIn(2000);
+    }
+    
+ 
+
+// SWIPER
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView:2,
+    slidesPerGroup:2,
+    spaceBetween:10,
+    loop: true, 
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+  });
+  var swiperBrand = new Swiper('.swiper-container2', {
+    slidesPerView:3,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+  });
+
 }
+
+
 
 
     // if($('.nav-depth2').css("display") == "block" && $('.nav-depth2').children().length !== 0) {
